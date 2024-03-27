332eb5e9dff24107a16bb9a1ec2db13ce32b7945
Chandan Kumar Singh <91355859+ChandanKumar-Singh@users.noreply.github.com>
2024-03-27 22:21:13 +0530

    reel_error

--- models/index.js
+++ models/index.js
@@ -1,29 +1,29 @@
-const AppSettingSchema = require('./appsetting');
-const Admin = require('./admin');
-const User = require('./user');
-const Wallet = require('./wallet');
-const Transaction = require('./transaction');
-const Reel = require('./Reel');
-const FeedModel = require('./feed');
-const FeedComment = require('./feed_comment');
-const Comment = require('./comments');
-const UserInteraction = require('./user-interaction');
-const Story = require('./story');
-const Sound = require('./sound');
+// const AppSettingSchema = require('./appsetting');
+// const Admin = require('./admin');
+// const User = require('./user');
+// const Wallet = require('./wallet');
+// const Transaction = require('./transaction');
+// const Reel = require('./Reel');
+// const FeedModel = require('./feed');
+// const FeedComment = require('./feed_comment');
+// const Comment = require('./comments');
+// const UserInteraction = require('./user-interaction');
+// const Story = require('./story');
+// const Sound = require('./sound');
 
 
 
-module.exports = {
-    AppSettingSchema,
-    Admin,
-    User,
-    Wallet,
-    Transaction, 
-    Reel,
-    FeedModel,
-    FeedComment,
-    Comment,
-    UserInteraction,
-    Story,
-    Sound
-};
+// module.exports = {
+//     AppSettingSchema,
+//     Admin,
+//     User,
+//     Wallet,
+//     Transaction, 
+//     Reel,
+//     FeedModel,
+//     FeedComment,
+//     Comment,
+//     UserInteraction,
+//     Story,
+//     Sound
+// };
